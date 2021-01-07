62e7b9f1b11283c66fbd477df03c4f95daac4afa
1190560 <1190560@isep.ipp.pt>
2021-01-07 19:16:07 +0000

    LAPR2AP2-19 #Development CourierRegistration [Implementation] LAPR2AP2-49 #Development GetDeliveryAddress [Implementation] LAPR2AP2-77 #Development AvailableChargingStation [Analysis]

--- sql/Functions and Procedures/Courier Management/US19_CourierRegistration.sql
+++ sql/Functions and Procedures/Courier Management/US19_CourierRegistration.sql
@@ -1,20 +1,20 @@
-CREATE OR REPLACE PROCEDURE addCourier(strName COURIER.NAME%type, strEmail "User".EMAIL%type, strPassword "User".PASSWORD%type,
-                                        strNif COURIER.NIF%type, strIban COURIER.IBAN%type) IS
+CREATE OR REPLACE PROCEDURE addCourier(strName "User".NAME%type, strEmail "User".EMAIL%type, strPassword "User".PASSWORD%type,
+                                        strNif "User".NIF%type, strIban COURIER.IBAN%type) IS
     courierValidation int;
     invalidInsertion exception;
     userId "User".id%type;
 BEGIN
 
 -- Validates existing Courier
-SELECT COUNT(co.NAME) INTO courierValidation
-FROM COURIER co
-WHERE co.NIF = strNif;
+SELECT COUNT(us.NAME) INTO courierValidation
+FROM "User" us
+WHERE us.NIF = strNif;
 
 IF courierValidation > 0 THEN
 RAISE invalidInsertion;
 END IF;
 
-SELECT COUNT(co.NAME) INTO courierValidation
+SELECT COUNT(co.IBAN) INTO courierValidation
 FROM COURIER co
 WHERE co.IBAN = strIban;
 
@@ -31,12 +31,12 @@ IF courierValidation > 0 THEN
 END IF;
 
 -- Creates a new User
-INSERT INTO "User"(EMAIL, PASSWORD)
-VALUES (strEmail, strPassword)
+INSERT INTO "User"(EMAIL,PASSWORD,NIF,NAME)
+VALUES (strEmail,strPassword,strNif,strName)
 RETURNING id into userId;
 
 -- Inserir Courier
-INSERT INTO COURIER(USERID, NAME, NIF, IBAN)
+INSERT INTO COURIER(USERID,IBAN)
 VALUES (userId,strName,strNif,strIban);
 
 EXCEPTION
--- sql/Functions and Procedures/Delivery Management/US49_getDeliveryAddress.sql
+++ sql/Functions and Procedures/Delivery Management/US49_getDeliveryAddress.sql
@@ -1,15 +1,17 @@
 CREATE OR REPLACE FUNCTION getDeliveryAddress(strEmail "User".EMAIL%type) RETURN ADDRESS%ROWTYPE IS
     address ADDRESS%ROWTYPE;
     address_not_found exception;
-begin
+BEGIN
 
-    select ADDRESS.* into address
-    from ADDRESS ad INNER JOIN "Order" ord on ad.ID = ord.ID /*CHANGE*/
+    SELECT AD.*, PD.*
+    FROM ADDRESS AD INNER JOIN "Order" ord on ad.ID = ord.ADDRESSID
+                    INNER JOIN ORDERPRODUCT OP on OP.ORDERID = ord.ID
+                    INNER JOIN PRODUCT PD on PD.ID = OP.PRODUCTID
                     INNER JOIN DELIVERY dv on dv.ORDERID = ord.ID
-                    INNER JOIN DELIVERYRUN D on dv.DELIVERYRUNID = D.DELIVERYORDERID
-                    INNER JOIN COURIER C2 on D.COURIERUSERID = C2.USERID
+                    INNER JOIN DELIVERYRUN D on dv.DELIVERYRUNID = D.ID
+                    INNER JOIN COURIER C2 on D.COURIERID = C2.USERID
                     INNER JOIN "User" us ON C2.USERID = us.ID
-    WHERE us.EMAIL = strEmail;
+    WHERE us.EMAIL = strEmail AND D.DELIVERYSTATUS = 'InProgress';
 
     IF address IS NULL THEN
         RAISE address_not_found;
@@ -22,3 +24,4 @@ EXCEPTION
         raise_application_error(-20025, 'Address Not Found!');
         RETURN NULL;
 END;
+
