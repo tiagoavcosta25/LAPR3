113ae47db4f839693892d7539cae22e1d6b7a26c
António Barbosa <1190404@isep.ipp.pt>
2021-01-07 23:01:35 +0000

    LAPR2AP2-48 #development sql script [implementation]

--- sql/Functions and Procedures/Delivery Management/US48_getOrderByCourier.sql.sql
+++ sql/Functions and Procedures/Delivery Management/US48_getOrderByCourier.sql.sql
@@ -4,13 +4,15 @@ create or replace function getOrderByCourier(p_email "User".EMAIL%type) RETURN S
 begin
     open v_order for
         select O.DESCRIPTION, O.ORDERSTATUS, O.ORDERDATE, O.TOTALWEIGHT, O.AMOUNT, O.ADDITIONALFEE,
-               C.CREDITS, U.*, A.*, CC.*
+       C.CREDITS, U.*, A.*, CC.*, op.quantity, P.*
         from "Order" O
                  inner join CLIENT C on C.USERID = O.CLIENTID
                  inner join CREDITCARDCLIENT CCC on CCC.CLIENTID = C.USERID
                  inner join CREDITCARD CC on CCC.CREDITCARDNR = CC.CREDITCARDNR
                  inner join ADDRESS A on C.ADDRESSID = A.ID
                  inner join "User" U on C.USERID = U.ID
+                 inner join ORDERPRODUCT OP on O.ID = OP.ORDERID
+                 inner join PRODUCT P on P.id = OP.PRODUCTID
         where U.EMAIL = p_email;
 
     if v_order is null then
