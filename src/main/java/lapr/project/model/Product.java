dab8bff7c222a904adbeb959391f515d4dc0e253
1191460 <1191460@isep.ipp.pt>
2021-01-07 02:07:39 +0000

    LAPR2AP2-70 #close Product Registration [Implementation]

--- src/main/java/lapr/project/model/Product.java
+++ src/main/java/lapr/project/model/Product.java
@@ -3,45 +3,59 @@ package lapr.project.model;
 import java.util.Objects;
 
 public class Product {
-    private String productId;
-    private String name;
-    private String description;
-    private double unitaryPrice;
-    private double unitaryWeight;
+    private int m_intId;
+    private String m_strName;
+    private String m_strDescription;
+    private float m_fltUnitaryPrice;
+    private float m_fltUnitaryWeight;
 
-    public Product(String productId, String name, String description, double unitaryPrice, double unitaryWeight) {
-        this.productId = productId;
-        this.name = name;
-        this.description = description;
-        this.unitaryPrice = unitaryPrice;
-        this.unitaryWeight = unitaryWeight;
+    private static int DEFAULT_ID = -1;
+    private static String DEFAULT_NAME = "";
+    private static String DEFAULT_DESCRIPTION = "";
+    private static float DEFAULT_UNITARY_PRICE = 0;
+    private static float DEFAULT_UNITARY_WEIGHT = 0;
+
+    public Product(int m_intId, String m_strName, String m_strDescription, float m_fltUnitaryPrice, float m_fltUnitaryWeight) {
+        this.m_intId = m_intId;
+        this.m_strName = m_strName;
+        this.m_strDescription = m_strDescription;
+        this.m_fltUnitaryPrice = m_fltUnitaryPrice;
+        this.m_fltUnitaryWeight = m_fltUnitaryWeight;
     }
 
-    public Product(String name, String description, double unitaryPrice, double unitaryWeight) {
-        this.name = name;
-        this.description = description;
-        this.unitaryPrice = unitaryPrice;
-        this.unitaryWeight = unitaryWeight;
+    public Product(String m_strName, String m_strDescription, float m_fltUnitaryPrice, float m_fltUnitaryWeight) {
+        this.m_strName = m_strName;
+        this.m_strDescription = m_strDescription;
+        this.m_fltUnitaryPrice = m_fltUnitaryPrice;
+        this.m_fltUnitaryWeight = m_fltUnitaryWeight;
     }
 
-    public String getProductId() {
-        return productId;
+    public Product() {
+        this.m_intId = DEFAULT_ID;
+        this.m_strName = DEFAULT_NAME;
+        this.m_strDescription = DEFAULT_DESCRIPTION;
+        this.m_fltUnitaryPrice = DEFAULT_UNITARY_PRICE;
+        this.m_fltUnitaryWeight = DEFAULT_UNITARY_WEIGHT;
     }
 
-    public String getName() {
-        return name;
+    public int getM_intId() {
+        return m_intId;
     }
 
-    public String getDescription() {
-        return description;
+    public String getM_strName() {
+        return m_strName;
     }
 
-    public double getUnitaryPrice() {
-        return unitaryPrice;
+    public String getM_strDescription() {
+        return m_strDescription;
     }
 
-    public double getUnitaryWeight() {
-        return unitaryWeight;
+    public float getM_fltUnitaryPrice() {
+        return m_fltUnitaryPrice;
+    }
+
+    public float getM_fltUnitaryWeight() {
+        return m_fltUnitaryWeight;
     }
 
     @Override
@@ -49,11 +63,11 @@ public class Product {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
-        return Objects.equals(productId, product.productId);
+        return m_intId == product.m_intId;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(productId);
+        return Objects.hash(m_intId);
     }
 }
