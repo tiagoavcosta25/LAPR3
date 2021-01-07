de4367a53a061759ed872424c688e092437e64a5
1190967 <1190967@isep.ipp.pt>
2021-01-07 00:26:26 +0000

    LAPR2AP2-25 #developing Make An Order [Implementation]

--- src/main/java/lapr/project/model/Order.java
+++ src/main/java/lapr/project/model/Order.java
@@ -2,6 +2,7 @@ package lapr.project.model;
 
 import oracle.sql.DATE;
 
+import java.sql.Date;
 import java.util.Objects;
 
 public class Order {
@@ -9,20 +10,24 @@ public class Order {
     private float m_fltAmount;
     private float m_fltTotalWeight;
     private float m_fltAdditionalFee;
-    private DATE m_dtOrderDate;
+    private Date m_dtOrderDate;
     private String m_strDescription;
     private String m_strStatus;
+    private Client m_oClient;
+    private Address m_oAddress;
 
     private static int DEFAULT_ID = -1;
     private static float DEFAULT_AMOUNT = 0;
     private static float DEFAULT_TOTAL_WEIGHT = 0;
     private static float DEFAULT_ADDITIONAL_FEE = 0;
-    private static DATE DEFAULT_DATE = new DATE();
+    private static Date DEFAULT_DATE = null;
     private static String DEFAULT_DESCRIPTION = "";
     private static String DEFAULT_STATUS = "";
+    private static Client DEFAULT_CLIENT = new Client();
+    private static Address DEFAULT_ADDRESS = new Address();
 
-    public Order(int intId, float fltAmount, float fltTotalWeight, float fltAdditionalFee, DATE dtOrderDate,
-                 String strDescription, String strStatus) {
+    public Order(int intId, float fltAmount, float fltTotalWeight, float fltAdditionalFee, Date dtOrderDate,
+                 String strDescription, String strStatus, Client oClient, Address oAddress) {
         this.m_intId = intId;
         this.m_fltAmount = fltAmount;
         this.m_fltTotalWeight = fltTotalWeight;
@@ -30,10 +35,12 @@ public class Order {
         this.m_dtOrderDate = dtOrderDate;
         this.m_strDescription = strDescription;
         this.m_strStatus = strStatus;
+        this.m_oClient = oClient;
+        this.m_oAddress = oAddress;
     }
 
-    public Order(float fltAmount, float fltTotalWeight, float fltAdditionalFee, DATE dtOrderDate,
-                 String strDescription, String strStatus) {
+    public Order(float fltAmount, float fltTotalWeight, float fltAdditionalFee, Date dtOrderDate,
+                 String strDescription, String strStatus, Client oClient, Address oAddress) {
         this.m_intId = DEFAULT_ID;
         this.m_fltAmount = fltAmount;
         this.m_fltTotalWeight = fltTotalWeight;
@@ -41,6 +48,8 @@ public class Order {
         this.m_dtOrderDate = dtOrderDate;
         this.m_strDescription = strDescription;
         this.m_strStatus = strStatus;
+        this.m_oClient = oClient;
+        this.m_oAddress = oAddress;
     }
 
     public Order() {
@@ -51,6 +60,8 @@ public class Order {
         this.m_dtOrderDate = DEFAULT_DATE;
         this.m_strDescription = DEFAULT_DESCRIPTION;
         this.m_strStatus = DEFAULT_STATUS;
+        this.m_oClient = DEFAULT_CLIENT;
+        this.m_oAddress = DEFAULT_ADDRESS;
     }
 
     public int getId() {
@@ -85,11 +96,11 @@ public class Order {
         this.m_fltAdditionalFee = fltAdditionalFee;
     }
 
-    public DATE getOrderDate() {
+    public Date getOrderDate() {
         return m_dtOrderDate;
     }
 
-    public void setOrderDate(DATE dtOrderDate) {
+    public void setOrderDate(Date dtOrderDate) {
         this.m_dtOrderDate = dtOrderDate;
     }
 
@@ -109,17 +120,20 @@ public class Order {
         this.m_strStatus = strStatus;
     }
 
-    @Override
-    public String toString() {
-        return "Order{" +
-                "m_intId=" + m_intId +
-                ", m_fltAmount=" + m_fltAmount +
-                ", m_fltTotalWeight=" + m_fltTotalWeight +
-                ", m_fltAdditionalFee=" + m_fltAdditionalFee +
-                ", m_dtOrderDate=" + m_dtOrderDate +
-                ", m_strDescription='" + m_strDescription + '\'' +
-                ", m_strStatus='" + m_strStatus + '\'' +
-                '}';
+    public Client getClient() {
+        return m_oClient;
+    }
+
+    public void setClient(Client m_oClient) {
+        this.m_oClient = m_oClient;
+    }
+
+    public Address getAddress() {
+        return m_oAddress;
+    }
+
+    public void setAddress(Address m_oAddress) {
+        this.m_oAddress = m_oAddress;
     }
 
     @Override
@@ -133,5 +147,20 @@ public class Order {
     @Override
     public int hashCode() {
         return Objects.hash(m_intId);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "m_intId=" + m_intId +
+                ", m_fltAmount=" + m_fltAmount +
+                ", m_fltTotalWeight=" + m_fltTotalWeight +
+                ", m_fltAdditionalFee=" + m_fltAdditionalFee +
+                ", m_dtOrderDate=" + m_dtOrderDate +
+                ", m_strDescription='" + m_strDescription + '\'' +
+                ", m_strStatus='" + m_strStatus + '\'' +
+                ", m_oClient=" + m_oClient +
+                ", m_oAddress=" + m_oAddress +
+                '}';
     }
 }
