f90e23638c24368c0478f77e18b6fff26f05a29c
1190967 <1190967@isep.ipp.pt>
2021-01-07 12:50:46 +0000

    LAPR2AP2-25 #developing Make An Order [Review]

---
+++ sql/US25_AddOrder.sql
@@ -0,0 +1,59 @@
+create or replace procedure addOrder(p_name "Order".name%type, p_nif Client.nif%type, p_credits Client.credits%type,
+                                      p_latitude Address.latitude%type, p_longitude Address.longitude%type,
+                                      p_streetName Address.streetName%type, p_doorNumber Address.doorNumber%type,
+                                      p_postalCode Address.postalCode%type, p_locality Address.locality%type,
+                                      p_country Address.country%type, p_creditCardNr CreditCard.creditCardNr%type,
+                                      p_validityDate CreditCard.validityDate%type, p_ccv CreditCard.CCV%type,
+                                      p_email "User".email%type, p_password "User".password%type)
+    is
+    userId            "User".id%type;
+    addressIdentifier Address.id%type;
+    creditCardId      CreditCard.creditCardNr%type;
+begin
+
+    -- Creates a new User
+    Insert into "User"(EMAIL, PASSWORD)
+    Values (p_email, p_password)
+    returning id into userId;
+
+-- Creates a new Address
+    select id
+    into addressIdentifier
+    from ADDRESS
+    where LATITUDE = p_latitude
+      and LONGITUDE = p_longitude
+      and DOORNUMBER = p_doorNumber
+      and STREETNAME = p_streetName
+      and POSTALCODE = p_postalCode
+      and LOCALITY = p_locality
+      and COUNTRY = p_country;
+
+    if addressIdentifier is null then
+        Insert into Address(LATITUDE, LONGITUDE, DOORNUMBER, STREETNAME, POSTALCODE, LOCALITY, COUNTRY)
+        Values (p_latitude, p_longitude, p_doorNumber, p_streetName, p_postalCode, p_locality, p_country)
+        returning id into addressIdentifier;
+    end if;
+
+    -- Creates a new Credit Card
+-- VERIFICAR SE CREDIT CARD JA EXISTE, SE SIM SALTAR ESTA PARTE À FRENTE
+    select CREDITCARDNR
+    into creditCardId
+    from CREDITCARD
+    where CREDITCARDNR = p_creditCardNr;
+
+    if creditCardId is null then
+        Insert into CreditCard(CREDITCARDNR, VALIDITYDATE, CCV)
+        Values (p_creditCardNr, p_validityDate, p_ccv)
+        returning CREDITCARDNR into creditCardId;
+    end if;
+
+-- Creates a new Credit Card Client
+    Insert into CreditCard_Client(CREDITCARDCREDITCARDNR, CLIENTUSERID)
+    Values (creditCardId, userId);
+
+-- Creates a new Client
+    Insert into Client(userId, NAME, NIF, CREDITS, ADDRESSID)
+    Values (p_name, p_nif, p_nif, p_credits, addressIdentifier);
+
+
+end;
--- src/main/java/lapr/project/model/registration/PharmacyRegistration.java
+++ src/main/java/lapr/project/model/registration/PharmacyRegistration.java
@@ -1,9 +1,15 @@
 package lapr.project.model.registration;
 
 import lapr.project.data.DataHandler;
+import lapr.project.model.Address;
+import lapr.project.model.Client;
+import lapr.project.model.Order;
 import lapr.project.model.Pharmacy;
+import oracle.jdbc.OracleTypes;
 
 import java.sql.CallableStatement;
+import java.sql.Date;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PharmacyRegistration extends DataHandler {
@@ -16,10 +22,44 @@ public class PharmacyRegistration extends DataHandler {
      * @param intChargingSlots  Pharmacy's charging slots
      * @return True if Pharmacy was registered, false if otherwise
      */
-    public boolean registerNewPharmacy(String strName, int intParkingSlots, int intChargingSlots) {
-        Pharmacy pharmacy = new Pharmacy(strName, intParkingSlots, intChargingSlots);
+    public boolean registerNewPharmacy(String strName, float latitude, float longitude, String streetName,
+                                       String doorNumber, String postalCode, String locality, String country) {
+        Pharmacy pharmacy = new Pharmacy(strName, new Address(latitude, longitude, streetName, doorNumber, postalCode, locality, country));
         return addPharmacyToDB(pharmacy);
 
+    }
+
+    public Pharmacy getPharmacy(int id) {
+
+        CallableStatement callStmt = null;
+        try {
+            callStmt = getConnection().prepareCall("{ ? = call getPharmacy(?) }");
+
+            callStmt.registerOutParameter(1, OracleTypes.CURSOR);
+            callStmt.setInt(id, 1);
+
+            callStmt.execute();
+
+            ResultSet rSet = (ResultSet) callStmt.getObject(1);
+
+            if (rSet.next()) {
+
+                int intId = rSet.getInt(1);
+                String strName = rSet.getString(2);
+                Float fltLatitude = rSet.getFloat(4);
+                Float fltLongitude = rSet.getFloat(5);
+                String strStreetName = rSet.getString(6);
+                String strDoorNumber = rSet.getString(7);
+                String strPostalCode = rSet.getString(8);
+                String strLocality = rSet.getString(9);
+                String strCountry = rSet.getString(10);
+
+                return new Pharmacy(intId, strName, new Address(fltLatitude, fltLongitude, strStreetName, strDoorNumber, strPostalCode, strLocality, strCountry));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        throw new IllegalArgumentException("No Order with ID:" + id);
     }
 
 
@@ -29,19 +69,24 @@ public class PharmacyRegistration extends DataHandler {
 
 
     public boolean addPharmacyToDB(Pharmacy p) {
-        return addPharmacyToDB(p.getName(), p.getParkingSlots(), p.getChargingSlots());
+        return addPharmacyToDB(p.getName(), p.getAddress());
     }
 
-    private boolean addPharmacyToDB(String strName, int intParkingSlots, int intChargingSlots) {
+    private boolean addPharmacyToDB(String strName, Address oAddress) {
         boolean flag = true;
         try {
             openConnection();
 
-            CallableStatement callStmt = getConnection().prepareCall("{ call addProduct(?,?,?,?,?) }");
+            CallableStatement callStmt = getConnection().prepareCall("{ call addPharmacy(?,?,?,?,?) }");
 
             callStmt.setString(1, strName);
-            callStmt.setInt(2, intParkingSlots);
-            callStmt.setInt(3, intChargingSlots);
+            callStmt.setFloat(2, oAddress.getM_latitude());
+            callStmt.setFloat(3, oAddress.getM_longitude());
+            callStmt.setString(4, oAddress.getM_streetName());
+            callStmt.setString(5, oAddress.getM_doorNumber());
+            callStmt.setString(6, oAddress.getM_postalCode());
+            callStmt.setString(7, oAddress.getM_locality());
+            callStmt.setString(8, oAddress.getM_country());
 
             callStmt.execute();
 
