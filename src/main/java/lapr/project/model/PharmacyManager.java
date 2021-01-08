e0fa9f638d6be406bd32eefdb7505da3ef46f593
Pedro Santos <1190967@isep.ipp.pt>
2021-01-08 18:11:04 +0000

    LAPR2AP2-167 #developing Register Pharmacy [Review]

--- src/main/java/lapr/project/model/PharmacyManager.java
+++ src/main/java/lapr/project/model/PharmacyManager.java
@@ -1,7 +1,6 @@
 package lapr.project.model;
 
 import java.security.NoSuchAlgorithmException;
-import java.util.Date;
 
 public class PharmacyManager extends User{
 
@@ -9,7 +8,11 @@ public class PharmacyManager extends User{
         super();
     }
 
-    public PharmacyManager(String name, Integer nif, String email, String password) throws NoSuchAlgorithmException {
+    public PharmacyManager(int intId, String email, String password, Integer nif, String name) throws NoSuchAlgorithmException {
+        super(intId, email, password,nif,name);
+    }
+
+    public PharmacyManager(String email, String password, Integer nif, String name) throws NoSuchAlgorithmException {
         super(email, password,nif,name);
     }
 }
