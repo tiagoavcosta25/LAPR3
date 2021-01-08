fe5392ed1ba42796826c4d4737afffc05ae53df3
1190967 <1190967@isep.ipp.pt>
2021-01-08 02:37:03 +0000

    LAPR2AP2-155 #developing Generate Invoice [Review]

--- sql/Functions and Procedures/Make An Order/US26_GetLatestOrder.sql
+++ sql/Functions and Procedures/Make An Order/US26_GetLatestOrder.sql
@@ -1,25 +1,30 @@
-create or replace function getClientByEmail(p_email "User".EMAIL%type)
+create or replace function getLatestOrder(p_clientId "User".ID%type)
     return "Order"%rowtype is
-    v_client Client%rowtype;
-    client_not_found exception;
+    v_order "Order"%rowtype;
+    v_orderId "Order".id%type;
+    order_not_found exception;
 begin
 
-    select "User".*, CLIENT.CREDITS, ADDRESS.*
-    into v_client
-    from CLIENT
-    inner join "User" on CLIENT.USERID = "User".ID
-    inner join ADDRESS on CLIENT.ADDRESSID = ADDRESS.ID
-    where "User".EMAIL = p_email;
+    select max(id)
+    into v_orderId
+    from "Order";
 
-    if v_client is null then
-        raise client_not_found;
+    select id, description, orderstatus, orderdate, totalweight, amount, additionalfee, ADDRESS.*
+    into v_order
+    from "Order"
+    inner join ADDRESS on "Order".ADDRESSID = ADDRESS.ID
+    where "Order".id = v_orderId
+    and "Order".CLIENTID = p_clientId;
+
+    if v_order is null then
+        raise order_not_found;
     end if;
 
-    return v_client;
+    return v_order;
 
 EXCEPTION
-    when client_not_found then
-        raise_application_error(-20025, 'Client Not Found!');
+    when order_not_found then
+        raise_application_error(-20025, 'Order Not Found!');
         return null;
 
 end;
