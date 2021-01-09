edc85b8f4fb3ea71ec511fa05a3663e3cfbc4b16
1190560 <1190560@isep.ipp.pt>
2021-01-09 19:01:00 +0000

    LAPR2AP2-24 #developing SeeSuitableScooter [Implementation]

---
+++ src/main/java/lapr/project/controller/SeeSuitableScooterController.java
@@ -0,0 +1,54 @@
+package lapr.project.controller;
+
+import lapr.project.model.ChargingSlot;
+import lapr.project.model.Platform;
+import lapr.project.model.Scooter;
+import lapr.project.model.registration.CourierRegistration;
+import lapr.project.model.registration.DeliveryRegistration;
+import lapr.project.model.registration.ScooterRegistration;
+
+/**
+ * Register Courier Controller.
+ * <p>
+ * Group: Team Lisa [G-037]
+ * ______________________________________________________
+ *
+ * @author Ernesto Rodrigues <1190560@isep.ipp.pt>
+ * @author Jessica Alves <1190682@isep.ipp.pt>
+ * @author Pedro Santos <1190967@isep.ipp.pt>
+ * @author Rodrigo Costa <1191014@isep.ipp.pt>
+ */
+public class SeeSuitableScooterController {
+    /**
+     * Platform class instance
+     */
+    private Platform m_oPlatform;
+    /**
+     * Courier Management class
+     */
+    private DeliveryRegistration oDeliveryRegistration;
+    /**
+     * Courier Management class
+     */
+    private ScooterRegistration oScooterRegistration;
+
+    /**
+     * An empty constructor of RegisterCourierController that initiates the platform variable by getting it from the ApplicationPOT.
+     */
+    public SeeSuitableScooterController() {
+        this.m_oPlatform = ApplicationPOT.getInstance().getPlatform();
+    }
+
+    public Scooter getSuitableScooter(){
+        m_oPlatform = ApplicationPOT.getInstance().getPlatform();
+        /*UserSession session = ApplicationPOT.getInstance().getCurrentSession();
+        String email = session.getCurrentUserEmail();*/
+        String email = "user6@gmail.com";
+        oDeliveryRegistration = m_oPlatform.getDelReg();
+        oScooterRegistration = m_oPlatform.getScooterReg();
+        float deliveryEnergy = oDeliveryRegistration.getDeliveryEnergy();
+        return oScooterRegistration.getSuitableScooter(deliveryEnergy,email);
+    }
+
+}
+
