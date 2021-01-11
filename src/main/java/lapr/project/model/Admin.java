d085f5beeb8041240ecd3e84d76f66a367bf0466
1190560 <1190560@isep.ipp.pt>
2021-01-11 04:14:19 +0000

    LAPR2AP2-[All Issues] Admin Tests

--- src/main/java/lapr/project/model/Admin.java
+++ src/main/java/lapr/project/model/Admin.java
@@ -1,4 +1,34 @@
 package lapr.project.model;
 
-public class Admin {
+import java.security.NoSuchAlgorithmException;
+
+public class Admin extends User {
+
+    private Integer id;
+
+    public Admin() {
+        super();
+    }
+
+    public Admin(String strName, String strEmail, String strPassword, Integer strNif) throws NoSuchAlgorithmException {
+        super(strEmail, strPassword, strNif, strName);
+    }
+
+    public Admin(Integer intId,String strName, String strEmail, String strPassword, Integer strNif) throws NoSuchAlgorithmException {
+        super(strEmail, strPassword, strNif, strName);
+        this.setId(intId);
+    }
+
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
+
+
---
+++ src/test/java/lapr/project/model/AdminTest.java
@@ -0,0 +1,35 @@
+package lapr.project.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminTest {
+    Admin a1;
+    Admin a2;
+    Admin a3;
+
+    public AdminTest() throws NoSuchAlgorithmException {
+        a1 = new Admin();
+        a2 = new Admin("test","email@gmail.com","pass",123456789);
+        a3 = new Admin(1,"test","email@gmail.com","pass",123456789);
+    }
+
+    @Test
+    void getM_id() {
+        Integer expected = 1;
+        Integer real = a3.getId();
+        assertEquals(expected, real);
+    }
+
+    @Test
+    void setM_id() {
+        a3.setId(2);
+        Integer expected = 2;
+        Integer real = a3.getId();
+        assertEquals(expected,real);
+    }
+
+}
