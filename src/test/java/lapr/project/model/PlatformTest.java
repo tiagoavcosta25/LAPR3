8f42147930b3edd0f2c502a1cae32ec7445597cf
1191460 <1191460@isep.ipp.pt>
2021-01-11 08:11:36 +0000

    LAPR2AP2-110 #fixing Product Registration [Implementation]

--- src/test/java/lapr/project/model/PlatformTest.java
+++ src/test/java/lapr/project/model/PlatformTest.java
@@ -1,6 +1,6 @@
 package lapr.project.model;
 
-import lapr.project.data.registration.UserRegistration;
+import lapr.project.data.registration.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,4 +15,48 @@ class PlatformTest {
         assertNotEquals(null, result);
         assertNotEquals(result, null);
     }
+
+    @Test
+    void getClientReg() {
+        ClientRegistration result = platform.getClientReg();
+        assertNotEquals(null, result);
+        assertNotEquals(result, null);
+    }
+
+    @Test
+    void getCourReg() {
+        CourierRegistration result = platform.getCourReg();
+        assertNotEquals(null, result);
+        assertNotEquals(result, null);
+    }
+
+    @Test
+    void getOrderReg() {
+        OrderRegistration result = platform.getOrderReg();
+        assertNotEquals(null, result);
+        assertNotEquals(result, null);
+    }
+
+    @Test
+    void getInvoiceReg() {
+        InvoiceRegistration result = platform.getInvoiceReg();
+        assertNotEquals(null, result);
+        assertNotEquals(result, null);
+    }
+
+    @Test
+    void getScooterReg() {
+        ScooterRegistration result = platform.getScooterReg();
+        assertNotEquals(null, result);
+        assertNotEquals(result, null);
+    }
+
+    @Test
+    void getDelReg() {
+        DeliveryRegistration result = platform.getDelReg();
+        assertNotEquals(null, result);
+        assertNotEquals(result, null);
+    }
+
+
 }
