935ac01f9ec2be969b46a8453e32a221e4ffbe87
António Barbosa <1190404@isep.ipp.pt>
2021-01-12 15:38:27 +0000

    LAPR2AP2-122 #refinement fixing getShortestPathMethod and unit tests[Implementation]

--- src/test/java/lapr/project/controller/CalculateMostEfficientPathControllerTest.java
+++ src/test/java/lapr/project/controller/CalculateMostEfficientPathControllerTest.java
@@ -31,9 +31,51 @@ class CalculateMostEfficientPathControllerTest {
 
     @BeforeEach
     void setUp() {
-        this.expectedTrue = new Pair<>(null, 10.0);
-        this.expectedList = new ArrayList<>();
+        this.calculateMostEfficientPathController = new CalculateMostEfficientPathController("", "", "");
+        this.mockDeliveryDB = Mockito.mock(DeliveryDB.class);
+        initMocks(this);
+    }
 
+    @Test
+    void getShortestPath() {
+        this.expectedTrue = new Pair<>(null, 10.0);
+        addAddress();
+        String email = "email6@gmail.com";
+        ApplicationPOT.getInstance().setCurrentSession(new UserSession("email6@gmail.com"));
+        when(mockDeliveryDB.getAddressesByDeliveryRunId(email)).thenReturn(expectedList);
+        this.pharmacyAddress = this.expectedList.get(this.expectedList.size() - 1);
+        when(mockDeliveryDB.calculateMostEfficientPath(this.pharmacyAddress, this.pharmacyAddress, this.expectedList)).thenReturn(expectedTrue);
+        double result = this.calculateMostEfficientPathController.getShortestPath();
+        assertEquals(expectedTrue.getValue(), result);
+    }
+
+    @Test
+    void getShortestPath2() {
+        String email = "email6@gmail.com";
+        double expResult = -1;
+        ApplicationPOT.getInstance().setCurrentSession(new UserSession("email6@gmail.com"));
+        when(mockDeliveryDB.getAddressesByDeliveryRunId(email)).thenReturn(expectedList);
+        this.pharmacyAddress = new Address();
+        when(mockDeliveryDB.calculateMostEfficientPath(this.pharmacyAddress, this.pharmacyAddress, this.expectedList)).thenReturn(null);
+        double result = this.calculateMostEfficientPathController.getShortestPath();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    void getShortestPath3() {
+        String email = "email6@gmail.com";
+        double expResult = -1;
+        addAddress();
+        ApplicationPOT.getInstance().setCurrentSession(new UserSession("email6@gmail.com"));
+        when(mockDeliveryDB.getAddressesByDeliveryRunId(email)).thenReturn(expectedList);
+        this.pharmacyAddress = this.expectedList.get(this.expectedList.size() - 1);
+        when(mockDeliveryDB.calculateMostEfficientPath(this.pharmacyAddress, this.pharmacyAddress, this.expectedList)).thenReturn(null);
+        double result = this.calculateMostEfficientPathController.getShortestPath();
+        assertEquals(expResult, result);
+    }
+
+    private void addAddress() {
+        this.expectedList = new ArrayList<>();
         expectedList.add(new Address(41.1111112, -8.6114856, "1", "789", "4747-857", "loc1", "Portugal"));
         expectedList.add(new Address(41.1414512, -8.5672412, "1", "123", "4747-857", "loc1", "Portugal"));
         expectedList.add(new Address(41.1516599, -8.5217484, "1", "456", "4747-857", "loc1", "Portugal"));
@@ -42,19 +84,5 @@ class CalculateMostEfficientPathControllerTest {
         expectedList.add(new Address(41.1155582, -8.6187858, "1", "169", "4747-857", "loc1", "Portugal"));
         expectedList.add(new Address(41.1784687, -8.6111664, "431", "R. Dr. António Bernardino de Almeida", "4200-072", "Porto", "Portugal"));
 
-        this.calculateMostEfficientPathController = new CalculateMostEfficientPathController("", "", "");
-        this.mockDeliveryDB = Mockito.mock(DeliveryDB.class);
-        initMocks(this);
-    }
-
-    @Test
-    void getShortestPath() {
-        String email = "email6@gmail.com";
-        ApplicationPOT.getInstance().setCurrentSession(new UserSession("email6@gmail.com"));
-        when(mockDeliveryDB.getAddressesByDeliveryRunId(email)).thenReturn(expectedList);
-        this.pharmacyAddress = this.expectedList.get(this.expectedList.size() - 1);
-        when(mockDeliveryDB.calculateMostEfficientPath(this.pharmacyAddress, this.pharmacyAddress, this.expectedList)).thenReturn(expectedTrue);
-        double result = this.calculateMostEfficientPathController.getShortestPath();
-        assertEquals(expectedTrue.getValue(), result);
     }
 }
