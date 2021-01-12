73dedab5f6a08a8083ff5aca0502e4eb2d0149a5
1191460 <1191460@isep.ipp.pt>
2021-01-12 23:36:17 +0000

    LAPR2AP2-111 #refining Product Registration [Review]

--- sql/Functions and Procedures/Product Management/US73_getProduct.sql
+++ sql/Functions and Procedures/Product Management/US73_getProduct.sql
@@ -1,11 +1,20 @@
 CREATE OR REPLACE FUNCTION getProduct(intId PRODUCT.ID%TYPE) RETURN SYS_REFCURSOR
     IS
     rf_cur sys_refcursor;
+    product_not_found exception;
 BEGIN
     open rf_cur for
         select *
         from PRODUCT
         where id = intId;
 
+    if v_cursor is null then
+        raise product_not_found;
+    end if;
+
     return rf_cur;
+EXCEPTION
+    when product_not_found then
+        raise_application_error(-20034, 'Product Not Found!');
+        return null;
 end;
