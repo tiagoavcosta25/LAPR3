8ae6b239813c849a2581b184874e59e9a3ae74e8
1191460 <1191460@isep.ipp.pt>
2021-01-15 20:37:27 +0000

    LAPR2AP2-277 #developing Scooter Charge Email [Implementation]

--- src/test/java/lapr/project/utils/EmailSenderTest.java
+++ src/test/java/lapr/project/utils/EmailSenderTest.java
@@ -2,6 +2,9 @@ package lapr.project.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailSenderTest {
