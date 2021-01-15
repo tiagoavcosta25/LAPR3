dbc80ca8a45eac8080c9c64ca40f9899532c4063
1191460 <1191460@isep.ipp.pt>
2021-01-15 21:38:09 +0000

    LAPR2AP2-277 #developing Scooter Charge Email [Implementation]

--- src/main/java/lapr/project/utils/DirectoryVerification.java
+++ src/main/java/lapr/project/utils/DirectoryVerification.java
@@ -10,22 +10,23 @@ public class DirectoryVerification {
     public static String verifyFileCreation(String path, String filter, int secTimeToTest) {
         int slept = 0;
         int timeToTest = secTimeToTest * 1000;
-        while(slept < timeToTest - SLEEP_TIME) {
+        while(slept <= timeToTest - SLEEP_TIME) {
             FilenameFilter fileFilter = (dir, name) -> name.endsWith(filter);
             File dir = new File(path);
-            if (!dir.isDirectory()) {
-                System.out.println("Path does not exist!");
-                return "";
-            }
-            String[] list = dir.list(fileFilter);
-            if (list.length == 0)
-                slept += SLEEP_TIME;
-            else
-                return list[0].substring(0, list[0].length() - filter.length());
             try {
+                if (!dir.isDirectory()) {
+                    System.out.println("Path does not exist!");
+                    throw new InterruptedException();
+                }
+                String[] list = dir.list(fileFilter);
+                if (list.length == 0)
+                    slept += SLEEP_TIME;
+                else
+                    return list[0].substring(0, list[0].length() - filter.length());
                 Thread.sleep(SLEEP_TIME);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                System.out.println("Path does not exist!");
+                return "";
             }
         }
         return "";
--- src/test/java/lapr/project/utils/DirectoryVerificationTest.java
+++ src/test/java/lapr/project/utils/DirectoryVerificationTest.java
@@ -8,12 +8,21 @@ class DirectoryVerificationTest {
 
     @Test
     void verifyFileCreation() {
-        String expResult = "Readme";
-        String result = DirectoryVerification.verifyFileCreation(".", ".md", 10);
+        //String expResult = "Readme";
+        //String result = DirectoryVerification.verifyFileCreation(".", ".md", 10);
+        //assertEquals(expResult, result);
+
+        String expResult = "";
+        String result = DirectoryVerification.verifyFileCreation("doesntexist/" ,"1", 200);
         assertEquals(expResult, result);
 
-        expResult = "";
-        result = DirectoryVerification.verifyFileCreation("/doesntexist" ,"1", 5);
+        result = DirectoryVerification.verifyFileCreation("doesntexist/" ,"1", 5);
         assertEquals(expResult, result);
+
+        result = DirectoryVerification.verifyFileCreation("doesntexist/" ,"1", 0);
+        assertEquals(expResult, result);
+
+        //result = DirectoryVerification.verifyFileCreation(".", ".nonexistant", 10);
+        //assertEquals(expResult, result);
     }
 }
