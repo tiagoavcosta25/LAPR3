c7bb28d6c4d63f70676c96484c81c8cc3be9e7cd
DESKTOP-30CBGBS\slow <1191014@isep.ipp.pt>
2021-01-18 02:05:50 +0000

    LAPR2AP2-67 #fixing Login [Review]

--- src/main/java/lapr/project/data/UserDB.java
+++ src/main/java/lapr/project/data/UserDB.java
@@ -2,11 +2,19 @@ package lapr.project.data;
 
 
 import lapr.project.model.UserSession;
+import lapr.project.data.DataHandler;
+import lapr.project.utils.EncryptPassword;
 import oracle.jdbc.internal.OracleTypes;
+
+import java.security.NoSuchAlgorithmException;
 import java.sql.CallableStatement;
 import java.sql.SQLException;
 
 public class UserDB extends DataHandler {
+
+    public UserDB() {
+        super();
+    }
 
     /**
      * Checks if the User that has the 'email' given by parameter
@@ -21,19 +29,19 @@ public class UserDB extends DataHandler {
         try {
             openConnection();
 
-            CallableStatement callStmt = getConnection().prepareCall("{ ? = call logIn(?,?) }");
+            CallableStatement callStmt = getConnection().prepareCall("{ call logIn(?,?,?) }");
 
             // Regista o tipo de dados SQL para interpretar o resultado obtido.
 
 
-            callStmt.setString(2, email);
-            callStmt.setString(3, password);
-            callStmt.registerOutParameter(1 , OracleTypes.INTEGER);
+            callStmt.setString(1, email);
+            callStmt.setString(2, password);
+            callStmt.registerOutParameter(3 , OracleTypes.INTEGER);
 
 
             callStmt.execute();
 
-            int role = callStmt.getInt(1);
+            int role = callStmt.getInt(3);
 
             if (role != -1) {
                 new UserSession(email,role);
@@ -46,5 +54,5 @@ public class UserDB extends DataHandler {
         }
         return flag;
     }
-    
+
 }
