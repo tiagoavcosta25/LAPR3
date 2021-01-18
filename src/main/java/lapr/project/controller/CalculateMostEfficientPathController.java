0a673cdde83ae0a7f55c64deb5e306d544b06655
1191460 <1191460@isep.ipp.pt>
2021-01-18 22:55:11 +0000

    LAPR2AP2-277 #fixing Scooter Charge Email [Implementation]

--- src/main/java/lapr/project/controller/CalculateMostEfficientPathController.java
+++ src/main/java/lapr/project/controller/CalculateMostEfficientPathController.java
@@ -18,19 +18,5 @@ public class CalculateMostEfficientPathController {
         this.oDeliveryRunService = new DeliveryRunService();
     }
 
-    public double getShortestPath() {
-        this.m_oUserSession = ApplicationPOT.getInstance().getCurrentSession();
-        String email = this.m_oUserSession.getCurrentUserEmail();
-        List<Address> list = this.oDeliveryRunService.getAddressesByDeliveryRunId(email);
-        if (list != null) {
-            Address a = list.get(list.size() - 1);
-            list.remove(list.size() - 1);
-            Pair<LinkedList<Address>, Double> result = this.oDeliveryRunService.calculateMostEfficientPath(a, a, list);
-            if (result == null) {
-                return -1;
-            }
-            return result.getValue();
-        }
-        return -1;
-    }
+
 }
