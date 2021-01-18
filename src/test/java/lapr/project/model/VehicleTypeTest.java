086b93fb965679d590b73d8ae5e24d8a4db8ba45
1191460 <1191460@isep.ipp.pt>
2021-01-18 22:44:34 +0000

    LAPR2AP2-277 #fixing Scooter Charge Email [Implementation]

---
+++ src/test/java/lapr/project/model/VehicleTypeTest.java
@@ -0,0 +1,53 @@
+package lapr.project.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTypeTest {
+
+    private VehicleType m_vehicleType;
+    private VehicleType m_emptyVehicleType;
+
+    VehicleTypeTest() {
+        m_vehicleType = VehicleType.SCOOTER;
+        m_emptyVehicleType = VehicleType.NOTDEFINED;
+    }
+
+    @Test
+    void getDesignation() {
+        String expected = "Scooter";
+        String real = m_vehicleType.getDesignation();
+        assertEquals(expected,real);
+    }
+
+    @Test
+    void testEquals() {
+        VehicleType d = VehicleType.SCOOTER;
+        assertEquals(d,m_vehicleType);
+
+        assertEquals(m_vehicleType,m_vehicleType);
+
+        d = VehicleType.NOTDEFINED;
+        assertNotEquals(d,m_vehicleType);
+
+        d = VehicleType.DRONE;
+        assertNotEquals(d,m_vehicleType);
+
+        String s = "";
+        assertNotEquals(s,m_vehicleType);
+
+        d = null;
+        assertNotEquals(d,m_vehicleType);
+    }
+
+    @Test
+    void testToString() {
+        String expected =  "DeliveryStatus{" +
+                    "m_strDesignation='" + m_vehicleType.getDesignation() + '\'' +
+                    '}';
+
+        String real = m_vehicleType.toString();
+        assertEquals(expected,real);
+    }
+}
