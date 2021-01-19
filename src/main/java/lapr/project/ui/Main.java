71acd32111049a83f139204d9df901ee0970cdf8
1191460 <1191460@isep.ipp.pt>
2021-01-19 22:32:21 +0000

    LAPR2AP2-277 #fixing Scooter Charge Email [Implementation]

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -1,18 +1,6 @@
 package lapr.project.ui;
 
 import lapr.project.controller.*;
-import lapr.project.data.ParkDB;
-import lapr.project.model.*;
-import lapr.project.ui.console.MakeAnOrderUI;
-import lapr.project.utils.DirectoryVerification;
-import lapr.project.utils.EnergyCalculator;
-import lapr.project.utils.FileReader;
-
-import java.security.NoSuchAlgorithmException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Nuno Bettencourt <nmb@isep.ipp.pt> on 24/05/16.
@@ -35,8 +23,7 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello world\n");
 
-
-        FileReader.readFiles();
+        
 
 
         System.out.println("\nGoodbye world");
