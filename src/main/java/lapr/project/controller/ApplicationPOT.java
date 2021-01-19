bc720f36ba1bcb11a8e57599e6dab20e5b9a4e29
1191460 <1191460@isep.ipp.pt>
2021-01-19 22:26:01 +0000

    LAPR2AP2-277 #fixing Scooter Charge Email [Implementation]

--- src/main/java/lapr/project/controller/ApplicationPOT.java
+++ src/main/java/lapr/project/controller/ApplicationPOT.java
@@ -1,22 +1,14 @@
 package lapr.project.controller;
 
 import lapr.project.model.UserSession;
-import lapr.project.model.WorldMap;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 public class ApplicationPOT {
 
     private UserSession m_currentSession;
-    private WorldMap m_worldMap;
 
     private static ApplicationPOT singleton = null;
 
     public ApplicationPOT() {
-        m_worldMap = new WorldMap();
     }
 
     public static ApplicationPOT getInstance()
@@ -48,9 +40,5 @@ public class ApplicationPOT {
     }
 
     public void clearCurrentSession() { m_currentSession = null; }
-
-    public WorldMap getWorldMap() {
-        return m_worldMap;
-    }
 
 }
