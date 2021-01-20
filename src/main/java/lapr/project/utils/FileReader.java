3543be391787eb8569fe3996819da0766029a1ba
DESKTOP-30CBGBS\slow <1191014@isep.ipp.pt>
2021-01-20 00:25:03 +0000

    LAPR3AP2-[ALL ISSUES] #fixing File Reader [Implementation]

--- src/main/java/lapr/project/utils/FileReader.java
+++ src/main/java/lapr/project/utils/FileReader.java
@@ -1,12 +1,12 @@
 package lapr.project.utils;
 
 import lapr.project.controller.RegisterClientController;
+import lapr.project.controller.RegisterPathController;
 import lapr.project.controller.RegisterPharmacyController;
 import lapr.project.controller.RegisterScooterController;
 import lapr.project.model.CreditCard;
 
 import java.io.File;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,12 +17,14 @@ import java.util.logging.Logger;
 public class FileReader {
 
     public static final String FILE_CLIENTS = "src/main/resources/files/clients.csv";
+    public static final String FILE_PATHS = "src/main/resources/files/paths.csv";
     public static final String FILE_SCOOTERS = "src/main/resources/files/escooters.csv";
     public static final String FILE_PHARMACIES = "src/main/resources/files/pharmacies.csv";
     private static final Logger LOGGER = Logger.getLogger(FileReader.class.getName());
 
     public static void readFiles() {
         readGenericFile(FILE_CLIENTS);
+        readGenericFile(FILE_PATHS);
     }
 
     public static void readGenericFile(String path) {
@@ -44,6 +46,9 @@ public class FileReader {
                             break;
                         case FILE_PHARMACIES:
                             readPharmacyFile(columns);
+                            break;
+                        case FILE_PATHS:
+                            readPathFile(columns);
                             break;
                     }
                 }
@@ -68,24 +73,22 @@ public class FileReader {
             LOGGER.log(Level.WARNING, "There was a problem in the Credit Card information!");
             return;
         }
-        String email = columns[0].trim();
-        String pw = columns[1].trim();
-        Integer nif = Integer.parseInt(columns[2].trim());
-        String name = columns[3].trim();
-        Double latitude = Double.parseDouble(columns[4].trim());
-        Double longitude = Double.parseDouble(columns[5].trim());
-        Double altitude = Double.parseDouble(columns[6].trim());
-        String streetName = columns[7].trim();
-        String doorNumber = columns[8].trim();
-        String postalCode = columns[9].trim();
-        String locality = columns[10].trim();
-        String country = columns[11].trim();
 
-        if (ctrl.registerNewClient(name,nif,email,pw,latitude,longitude,altitude,streetName,doorNumber,postalCode,
-                locality,country, lst)) {
+        if (ctrl.registerNewClient(columns[3].trim(),Integer.parseInt(columns[2].trim()),columns[0].trim(),columns[1].trim(),Double.parseDouble(columns[4].trim())
+                ,Double.parseDouble(columns[5].trim()),Double.parseDouble(columns[6].trim()),columns[7].trim(),columns[8].trim(),columns[9].trim(),
+                columns[10].trim(),columns[11].trim(), lst)) {
             LOGGER.log(Level.INFO, "Client registered with success!");
         } else LOGGER.log(Level.WARNING, "There was a problem registering a Client");
+    }
 
+    public static void readPathFile(String[] columns) {
+        RegisterPathController ctrl = new RegisterPathController();
+
+        if (ctrl.registerPath(Double.parseDouble(columns[0]),Double.parseDouble(columns[1]),Double.parseDouble(columns[2]),
+                Double.parseDouble(columns[3]),columns[4], Double.parseDouble(columns[5]),Double.parseDouble(columns[6]),
+                Double.parseDouble(columns[7]))) {
+            LOGGER.log(Level.INFO,"Path was registered with success!");
+        }else LOGGER.log(Level.WARNING,"There was a problem registering a Path");
     }
 
 
