752cc2572eaf1a0f769055594905433a79ff7441
DESKTOP-30CBGBS\slow <1191014@isep.ipp.pt>
2021-01-22 23:18:31 +0000

    LAPR3AP2-[ALL ISSUES] #fixing Mutation Testing

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -1,12 +1,9 @@
 package lapr.project.ui;
-
-import lapr.project.utils.FileReader;
-
 class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world\n");
-        FileReader.readFiles();
+
         System.out.println("\nGoodbye world");
     }
 }
