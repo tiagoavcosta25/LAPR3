c8eabd146dc0c76020be26f615b5993ea6ef7a0b
DESKTOP-30CBGBS\slow <1191014@isep.ipp.pt>
2021-01-22 23:45:46 +0000

    LAPR3AP2-[ALL ISSUES] #fixing Mutation Coverage

--- src/test/java/lapr/project/controller/ParkScooterControllerTest.java
+++ src/test/java/lapr/project/controller/ParkScooterControllerTest.java
@@ -1,29 +1,57 @@
 package lapr.project.controller;
 
 import lapr.project.model.UserSession;
+import lapr.project.model.service.CourierService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ParkScooterControllerTest {
-    ParkScooterController oParkScooterController;
+    @InjectMocks
+    private ParkScooterController oParkScooterController;
+    @Mock
+    private CourierService mockCourierService;
 
     public ParkScooterControllerTest() {
         this.oParkScooterController = new ParkScooterController();
     }
-    //TODO é preciso dar fix, este método envolve os ficheiros recebidos
-/*
+
+    @BeforeEach
+    void setUp() {
+        this.oParkScooterController = new ParkScooterController();
+        this.mockCourierService = Mockito.mock(CourierService.class);
+        initMocks(this);
+    }
+
+
     @Test
     void parkScooter() {
         ApplicationPOT.getInstance().setCurrentSession(new UserSession("email123@gmail.com", 3));
+
+        when (mockCourierService.parkScooter(10)).thenReturn(true);
         boolean result = oParkScooterController.parkScooter(10);
         assertTrue(result);
-    }*/
 
-    @Test
-    void parkScooter2() {
-        ApplicationPOT.getInstance().setCurrentSession(new UserSession("email123@gmail.com", 2));
-        boolean result = oParkScooterController.parkScooter(10);
+        when (mockCourierService.parkScooter(10)).thenReturn(false);
+        result = oParkScooterController.parkScooter(10);
+
         assertFalse(result);
+
+        when (mockCourierService.parkScooter(10)).thenThrow(new IllegalArgumentException());
+        result = oParkScooterController.parkScooter(10);
+
+        assertFalse(result);
+
+        ApplicationPOT.getInstance().setCurrentSession(new UserSession("email123@gmail.com", 1));
+        result = oParkScooterController.parkScooter(10);
+
+        assertFalse(result);
+
     }
 }
