cf11b10f3b60ace22b376782ee24ce88d026bcfa
António Barbosa <1190404@isep.ipp.pt>
2021-01-25 22:05:55 +0000

    LAPR2AP2-48 #fixing UI developing [Implementation]

---
+++ src/main/java/lapr/project/ui/console/KnowDeliveryUI.java
@@ -0,0 +1,31 @@
+package lapr.project.ui.console;
+
+import lapr.project.controller.ApplicationPOT;
+import lapr.project.controller.KnowDeliveryController;
+import lapr.project.model.Order;
+import lapr.project.model.UserSession;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class KnowDeliveryUI {
+
+    public static final Logger LOGGER = Logger.getLogger(KnowDeliveryUI.class.getName());
+
+    public void run() {
+        KnowDeliveryController oCtrl = new KnowDeliveryController();
+        Scanner sc = new Scanner(System.in);
+        do {
+            try {
+                System.out.print("Know delivery:\n\nCourier's Email: ");
+                String strEmail = sc.nextLine();
+                ApplicationPOT.getInstance().setCurrentSession(new UserSession(strEmail));
+                Order oOrder = oCtrl.getOrderByCour();
+                System.out.printf("For the Courier with the email %s, the order assigned is:\n%s%n", strEmail, oOrder.toString());
+            } catch (Exception e) {
+                LOGGER.log(Level.WARNING, "Error Finding the delivery.");
+            }
+        } while (true);
+    }
+}
