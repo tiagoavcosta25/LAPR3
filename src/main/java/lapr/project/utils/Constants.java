afc5ee0a24f8f001076ac20071e82a1155823273
António Barbosa <1190404@isep.ipp.pt>
2021-01-26 19:43:02 +0000

    LAPR2AP2-298 #fixing Readme Report and SD's[Review]

--- src/main/java/lapr/project/utils/Constants.java
+++ src/main/java/lapr/project/utils/Constants.java
@@ -11,11 +11,12 @@ public class Constants {
     public static final double AIR_DENSITY = 1.225;                     //  kg.m^-3
     public static final double DEFAULT_DRONE_AREA = 0.3;                //  m^2 (0.2 m radius rotor, PI*r^2*4)
     public static final double KILOWATTHOUR = 3600000.0;
-    public static final double DRAG_COEFFICIENT = 0.4;
-    public static final double DEFAULT_VELOCITY = 10;                   //  m/s
-    public static final double DEFAULT_COURIER_WEIGHT = 75;
+    public static final double DRAG_COEFFICIENT = 1.1;
+    public static final double DEFAULT_VELOCITY = 8.3;                   //  m/s
+    public static final double DEFAULT_COURIER_WEIGHT = 80;
+    public static final double MAX_DRONE_POWER = 3000;
     public static final double KINETIC_FRICTION_COEFFICIENT = 0.8;
-    public static final double DEFAULT_SCOOTER_AREA = 0.75;                // m^2
+    public static final double DEFAULT_SCOOTER_AREA = 0.3;                // m^2
     public static final double SCOOTER_TOTAL_WEIGHT = 10;
     public static final double DRONE_WIDTH = 0.2;
     public static final String ESTIMATE_FILE_PATH = "src-C/estimate_files/";
