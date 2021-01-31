49ab520dca362fcabbbb8af87946038efa22bf29
António Barbosa <1190404@isep.ipp.pt>
2021-01-31 04:36:28 +0000

    LAPR2AP2-298 #fixing Readme Report and SD's[Review]

--- src/main/java/lapr/project/ui/console/KnowDeliveryUI.java
+++ src/main/java/lapr/project/ui/console/KnowDeliveryUI.java
@@ -16,13 +16,10 @@ public class KnowDeliveryUI implements UI {
 
     public void run() {
         KnowDeliveryController oCtrl = new KnowDeliveryController();
-        Scanner sc = new Scanner(System.in);
             try {
-                System.out.print("Know delivery:\n\nCourier's Email: ");
-                String strEmail = sc.nextLine();
-                ApplicationPOT.getInstance().setCurrentSession(new UserSession(strEmail));
+                System.out.print("Know delivery:\n\n");
                 Order oOrder = oCtrl.getOrderByCour();
-                System.out.printf("For the Courier with the email %s, the order assigned is:%n%s%n", strEmail, oOrder.toString());
+                System.out.printf("For the Courier with the email %s, the order assigned is:%n%s%n", ApplicationPOT.getInstance().getCurrentSession(), oOrder.toString());
             } catch (Exception e) {
                 LOGGER.log(Level.WARNING, "Error Finding the delivery.");
             }
